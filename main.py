e7c33c2a12e13546ed49d5a44b5c2416d97ab9c3
sachinkolhar <sachinkolhar4@gmail.com>
2026-02-16 12:16:01 +0530

    Fix static QR generation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from database import collection
 from models import Product
 from utils import generate_uuid, hash_metadata, generate_qr_code
-
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
@@ -11,6 +11,9 @@ import os
 BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 @app.post("/register-product")
@@ -34,11 +37,15 @@ def register_product(product: Product):
 
     collection.insert_one(record)
 
+    qr_image_url = f"{BASE_URL}/static/{product_id}.png"
+
     return {
-        "message": "Product registered successfully",
-        "product_id": product_id,
-        "verification_url": verification_url
-    }
+    "message": "Product registered successfully",
+    "product_id": product_id,
+    "verification_url": verification_url,
+    "qr_image_url": qr_image_url
+}
+
 
 @app.get("/verify-product/{product_id}", response_class=HTMLResponse)
 def verify_product(request: Request, product_id: str):
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import uuid
 import hashlib
 import json
+import os
+import qrcode
 
 def generate_uuid():
     return str(uuid.uuid4())
@@ -9,8 +11,10 @@ def hash_metadata(data: dict):
     metadata_string = json.dumps(data, sort_keys=True)
     return hashlib.sha256(metadata_string.encode()).hexdigest()
 
-import qrcode
+
 
 def generate_qr_code(data: str, filename: str):
+    os.makedirs("static", exist_ok=True)
     qr = qrcode.make(data)
-    qr.save(f"{filename}.png")
+    qr.save(f"static/{filename}.png")
+
